5ed61177e7ba305d14fbe16c69cfb4da4a4d65be
JHPAT100 <sanhool@live.com.mx>
2019-03-26 23:23:04 -0500

    guardado de pantalla_principal

---
+++ app/src/main/java/com/jhpat/discere/pantalla_principal.java
@@ -0,0 +1,13 @@
+package com.jhpat.discere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pantalla_principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_principal);
+    }
+}
