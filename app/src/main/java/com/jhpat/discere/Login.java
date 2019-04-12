41d88558713bb834524b2e8a1d12346d52b9793b
JHPAT100 <sanhool@live.com.mx>
2019-04-12 15:39:56 -0500

    Integracion_Login_Final

---
+++ app/src/main/java/com/jhpat/discere/Login.java
@@ -0,0 +1,142 @@
+package com.jhpat.discere;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Login extends AppCompatActivity {
+
+    private EditText user, pass;
+    private Button mSubmit, mRegister;
+    private CardView logD;
+    private ProgressDialog pDialog;
+
+    // Clase JSONParser
+    JSONParser jsonParser = new JSONParser();
+
+
+    // si trabajan de manera local "localhost" :
+    // En windows tienen que ir, run CMD > ipconfig
+    // buscar su IP
+    // y poner de la siguiente manera
+    // "http://xxx.xxx.x.x:1234/cas/login.php";
+
+    private static final String LOGIN_URL = "http://puntosingular.mx/cas/login.php";
+
+    // La respuesta del JSON es
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+
+        // setup input fields
+        user = (EditText) findViewById(R.id.userD);
+        pass = (EditText) findViewById(R.id.passD);
+        logD =  (CardView)findViewById(R.id.cardViewL);
+
+        // setup buttons
+
+        // register listeners
+
+        logD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AttemptLogin().execute();
+            }
+        });
+    }
+
+
+
+    class AttemptLogin extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Login.this);
+            pDialog.setMessage("Attempting login...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            int success;
+            String username = user.getText().toString();
+            String password = pass.getText().toString();
+            try {
+                // Building Parameters
+                List params = new ArrayList();
+                params.add(new BasicNameValuePair("username", username));
+                params.add(new BasicNameValuePair("password", password));
+
+                Log.d("request!", "starting");
+                // getting product details by making HTTP request
+                JSONObject json = jsonParser.makeHttpRequest(LOGIN_URL, "POST",
+                        params);
+
+                // check your log for json response
+                Log.d("Login attempt", json.toString());
+
+                // json success tag
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    Log.d("Login Successful!", json.toString());
+                    // save user data
+                    SharedPreferences sp = PreferenceManager
+                            .getDefaultSharedPreferences(Login.this);
+                    Editor edit = sp.edit();
+                    edit.putString("username", username);
+                    edit.commit();
+
+                    Intent i = new Intent(Login.this, pantalla_principal.class);
+                    finish();
+                    startActivity(i);
+                    return json.getString(TAG_MESSAGE);
+                } else {
+                    Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+                    return json.getString(TAG_MESSAGE);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+            if (file_url != null) {
+                Toast.makeText(Login.this, file_url, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
