c6339e0946456f7d7fd03837858a612bc1c14b95
sarielpinto <48193176+sarielpinto@users.noreply.github.com>
2019-06-12 21:21:54 -0500

    PARTE DE ADRIAN

--- app/src/main/java/com/jhpat/discere/dialogo.java
+++ app/src/main/java/com/jhpat/discere/dialogo.java
@@ -2,6 +2,7 @@ package com.jhpat.discere;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Properties;
 
@@ -21,7 +29,18 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import cz.msebera.android.httpclient.Header;
+
 public class dialogo extends AppCompatActivity {
+
+    public String nombre_1_1;
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    TextView nombre, apellido, email,nombre_1,apellido_1;
+    public static String NAME1, LAST_NAME1, GENDER1, ID1, EMAIL1, TEL1, PASSWORD1;//CLASE
+    JSONObject jsonObject;
+
+    public  static String ID_USUARIO;
     Session session = null;
     ProgressDialog pdialog = null;
     Context context = null;
@@ -36,6 +55,8 @@ public class dialogo extends AppCompatActivity {
     String msgC ="Lo sentimos, tú sesión ha sido rechazada";
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +64,7 @@ public class dialogo extends AppCompatActivity {
 
 
         recibirDatos();
+        cargarP();
         context = this;
 
 
@@ -79,7 +101,7 @@ public class dialogo extends AppCompatActivity {
                     try{
                         Message message = new MimeMessage(session);
                         message.setFrom(new InternetAddress("testfrom354@gmail.com"));
-                        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(reciep));
+                        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(EMAIL1));
                         message.setSubject(sub);
                         message.setContent(msg, "text/html; charset=utf-8");
                         Transport.send(message);
@@ -133,7 +155,7 @@ public class dialogo extends AppCompatActivity {
                     try{
                         Message message = new MimeMessage(sessionC);
                         message.setFrom(new InternetAddress("testfrom354@gmail.com"));
-                        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(reciepC));
+                        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(EMAIL1));
                         message.setSubject(subC);
                         message.setContent(msgC, "text/html; charset=utf-8");
                         Transport.send(message);
@@ -160,7 +182,7 @@ public class dialogo extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-               finish();
+                finish();
 
             }
         });
@@ -186,5 +208,75 @@ public class dialogo extends AppCompatActivity {
         namess.setText("Fellow: " + nombreF);
         date.setText("Date: "+ fecha);
         timeset.setText("Time: " + horaInicio + " - " + horaFin );
-        }
+    }
+    public void datosc (String Correo)
+    {
+
+        AsyncHttpClient conexion = new AsyncHttpClient();
+        final String url ="http://puntosingular.mx/cas/usuario.php"; //la url del web service
+        final RequestParams requestParams =new RequestParams();
+        requestParams.add("correo",Correo); //envio el parametro
+        conexion.post(url, requestParams, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody)
+            {
+
+                if (statusCode==200) // Lo mismo que con LOGIN
+                {
+
+
+                    try {
+                        jsonObject = new JSONObject(new String(responseBody));
+                        //Apartir de aqui, les asigno a los editText el valor que obtengo del webservice
+
+
+
+                        email.setText(jsonObject.getJSONArray("datos").getJSONObject(0).getString("email"));
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                else
+                {
+
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error)
+            {
+
+
+
+            }
+
+
+
+
+        });
+
+
+    }//FIN DATOSSC
+
+    private  void cargarP()
+    {
+        SharedPreferences preferencia = getApplicationContext().getSharedPreferences("Credenciales", Context.MODE_PRIVATE);
+        EMAIL1 = preferencia.getString("EMAIL2", "NO EXISTE");
+
+
+
+        datosc(EMAIL1);
+
+
+
+
+
+
+    }//Fin cargar preferencias
 }
